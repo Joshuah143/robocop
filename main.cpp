2532f9e61f2b91c363fa5ef395da74df701c5945
Joshua <joshua.himmens@icloud.com>
2021-11-02 14:23:19 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,23 +15,36 @@
 // MotorGroup3          motor_group   3, 4            
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
+// note that ports 1 and 2 are left
+// 3 and 4 are right, order does within groups not matter
+
 #include "vex.h"
+#include "cmath"
 
 using namespace vex;
 
-int main() {
-  // Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
-  MotorGroup1.setStopping(brake);
-  MotorGroup3.setStopping(brake);
-  while (true){
+void standard_mode(){
+  while ( true ){
     MotorGroup1.setVelocity(Controller1.Axis3.position(percent), percent);
     MotorGroup3.setVelocity(Controller1.Axis2.position(percent), percent);
-    if (Controller1.ButtonX.pressing()){
+    if (Controller1.ButtonB.pressing()){
       MotorGroup1.stop();
       MotorGroup3.stop();
       break;
     }
+    wait(5, msec);
+  }
+}
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  // start config
+  MotorGroup1.setStopping(coast);
+  MotorGroup3.setStopping(coast);
+  Controller1.ButtonA.pressed(standard_mode);
+  Controller1.Screen.print("Press A to start");
+  while ( true ){
     wait(5, msec);
   }
 }
