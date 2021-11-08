edc8b099eb339a1b1ffd7eb9f38caedebe72dbef
Joshua <joshua.himmens@gmail.com>
2021-11-07 22:03:05 -0700

    functionalize everything

--- main.cpp
+++ main.cpp
@@ -19,9 +19,9 @@
 /* PORTS:
 T8 = 14
 T6 = 15
-T4 = 17
+T5 = 17
 T2 = 16
-T8 = 18
+T4 = 18
 T1 = 19
 */
 
@@ -35,7 +35,9 @@ namespace run_cofigs{
   double run_speed = 1;
   string mode_name;
   bool forward_ = false;
-  int spin_lift = 0; // 0=stay, 1=up, 2=down
+  int spin_lift = 0; // 0=stay, 2=up, 1=down
+  string prefix = "N"; //N=none, M=manual, A=autonomouse
+  string n_m_message = "";
 }
 
 #define screen_print Controller1.Screen.print
@@ -51,9 +53,15 @@ using namespace run_cofigs;
 
 void flash_speed(){
   screen_clear();
-  screen_print("%s - %.2f       ", mode_name.c_str(), run_speed);
   Brain.Screen.clearLine();
-  Brain.Screen.print("%s - %.2f", mode_name.c_str(), run_speed);
+  if (prefix == "M"){
+    screen_print("%s - %s - %.2f  ", prefix.c_str(), mode_name.c_str(), run_speed);
+    Brain.Screen.print("%s - %s - %.2f  ", prefix.c_str(), mode_name.c_str(), run_speed);
+  }
+  else {
+    screen_print("%s - %s", prefix.c_str(), n_m_message.c_str());
+    Brain.Screen.print("%s - %s ", prefix.c_str(), n_m_message.c_str());
+  }
 }
 
 void set_half_speed() {
@@ -82,21 +90,51 @@ void forward_set() {
 }
 
 void lift_up() {
-  spin_lift = 1;
+  spin_lift = 2;
 }
 
 void lift_down() {
-  spin_lift = 2;
+  spin_lift = 1;
 }
 
 void lift_stop() {
   spin_lift = 0;
 }
 
-int main() {
-  // Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
-  // start config
+void run_lift(){
+  int lift_spin_magnitude = 15; // in intager percent
+  if (spin_lift == 0) {
+    lift_motors.stop();
+  }
+  else if (spin_lift == 1) {
+    lift_motors.spin(fwd, lift_spin_magnitude, percent);
+  }
+  else if (spin_lift == 2) {
+    lift_motors.spin(reverse, lift_spin_magnitude, percent);
+  }
+}
+
+void run_drive(){
+  double left = Controller1.Axis3.position(percent);
+  double right = Controller1.Axis2.position(percent);
+  if (((fabs(left) >= 0.3) || (fabs(right) >= 0.3))) {
+    MotorGroup17.spin(fwd, left * run_speed, percent);
+    MotorGroup18.spin(fwd, right * run_speed, percent);
+    forward_ = false;
+  }
+  else if (forward_){
+    MotorGroup17.spin(fwd, run_speed * 100, percent);
+    MotorGroup18.spin(fwd, run_speed * 100, percent);
+    screen_clear();
+  }
+  else {
+    MotorGroup17.stop();
+    MotorGroup18.stop();
+  }
+}
+
+void manual(){
+  prefix = "M";
   left_wheels.setStopping(coast);
   right_wheels.setStopping(coast);
   lift_motors.setStopping(brake);
@@ -109,33 +147,28 @@ int main() {
   Controller1.ButtonR1.released(lift_stop);
   Controller1.ButtonR2.released(lift_stop);
   while ( true ){
-    double left = Controller1.Axis3.position(percent);
-    double right = Controller1.Axis2.position(percent);
-    if (((fabs(left) >= 0.3) || (fabs(right) >= 0.3))) {
-      MotorGroup17.spin(fwd, left * run_speed, percent);
-      MotorGroup18.spin(fwd, right * run_speed, percent);
-      forward_ = false;
+    run_drive();
+    run_lift();
+    if (Controller1.ButtonDown.pressing()) {
+      break;
     }
-    else if (forward_){
-      MotorGroup17.spin(fwd, run_speed * 100, percent);
-      MotorGroup18.spin(fwd, run_speed * 100, percent);
-      screen_clear();
-    }
-    else {
-      MotorGroup17.stop();
-      MotorGroup18.stop();
-    }
-    int lift_spin_magnitude = 15; // in intager percent
-    if (spin_lift == 0) {
-      lift_motors.stop();
-    }
-    else if (spin_lift == 1) {
-      lift_motors.spin(fwd, lift_spin_magnitude, percent);
-    }
-    else if (spin_lift == 2) {
-      lift_motors.spin(reverse, lift_spin_magnitude, percent);
-    }
-
     wait(5, msec);
+  }
+}
+
+void auto_() {
+  prefix = "A";
+  // TODO: add an auto section
+}
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  // start config
+  left_wheels.setStopping(coast);
+  right_wheels.setStopping(coast);
+  lift_motors.setStopping(brake);
+  while ( true ){
+    manual();
   }
 }
